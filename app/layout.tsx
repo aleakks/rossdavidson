85f4d886f1dfe696af05fe148e488016eaca2ca2
Cleo Stella Photography <alexfaccenda96@gmail.com>
2025-12-25 16:50:04 +0000

    Add Identity script to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </head>
       <body
         className={`${oswald.variable} ${inter.variable} antialiased`}
       >
